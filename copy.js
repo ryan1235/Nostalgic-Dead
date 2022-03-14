a5c86792e0da057d9aeacc847f1d864a07040be4
ryan luca <58185393+ryan1235@users.noreply.github.com>
2022-03-13 21:17:26 -0300

    agora vai

---
+++ copy.js
@@ -0,0 +1,4 @@
+function copy(){
+    let copiartexto = 'redemaniacraft.tk'
+    copiartexto.copy()
+}
--- online.js
+++ online.js
@@ -1,4 +1,4 @@
-    let url = 'https://mcstatus.snowdev.com.br/api/query/v3/redemaniacraft.tk '
+    let url = 'https://mcstatus.snowdev.com.br/api/query/v3/ntgdead.hostsquare.com.br'
 
     fetch(url).then(function(response){
         response.json().then(function(data){
@@ -7,11 +7,11 @@
 
 function statuss(dados){
     if(dados.online == true){
-        let online1 = document.getElementById("online1")
-        let online2 = document.getElementById("online2")
+        const online1 = document.getElementById("online1")
+        const online2 = document.getElementById("online2")
 
-        online1.innerHTML = `redemaniacraft.tk`
-        online2.innerHTML = `Servidor <strong id='on'>Online</strong> ${dados.players_online}/${dados.max_players} Players`
+        online1.innerHTML = `ntgdead.hostsquare.com.br`
+        online2.innerHTML = `Players <strong id='on'>Online</strong> ${dados.players_online}/${dados.max_players}`
     }else{
         online.innerHTML = `Servidor <strong id='off'>Servidor offiline</strong>`
     }
